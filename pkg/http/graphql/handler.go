ef74502d0a4c729b3c438f92ccac3e6fd2f54c2d
patipol <patipol.st@gmail.com>
2020-05-09 15:56:27 +0700

    Create gql handler fn

---
+++ pkg/http/graphql/handler.go
@@ -0,0 +1,30 @@
+package graphql
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/patipolst/go-demo/pkg/graph"
+	"github.com/patipolst/go-demo/pkg/graph/generated"
+)
+
+const defaultPort = "8080"
+
+// Run starts the graphql playground
+func Run() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
