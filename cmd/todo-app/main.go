86c326a2ea063f58940fdc2d33d9adb36ce98e4b
patipol <patipol.st@gmail.com>
2020-05-10 17:42:46 +0700

    Create a method wrapping store on a service

--- cmd/todo-app/main.go
+++ cmd/todo-app/main.go
@@ -1,19 +1,16 @@
 package main
 
 import (
-	"github.com/patipolst/go-demo/pkg/http/graphql"
-	"github.com/patipolst/go-demo/pkg/mutation"
-	"github.com/patipolst/go-demo/pkg/query"
+	"github.com/patipolst/go-demo/pkg/http/rest"
 	"github.com/patipolst/go-demo/pkg/service"
-	"github.com/patipolst/go-demo/pkg/store/db"
+	"github.com/patipolst/go-demo/pkg/store/memory"
 )
 
 func main() {
-	todoStore, _ := db.NewTodoStore()
-	// todoStore := memory.NewTodoStore()
-	todoQuery := query.NewTodoQuery(todoStore)
-	todoMutation := mutation.NewTodoMutation(todoStore)
-	todoService := service.NewTodoService(todoQuery, todoMutation)
-	graphql.Run(todoService)
-	// rest.Run(todoService)
+	// todoStore, _ := db.NewTodoStore()
+	todoStore := memory.NewTodoStore()
+	// todoService := service.NewTodoDBService(todoStore)
+	todoService := service.NewTodoMemoryService(todoStore)
+	// graphql.Run(todoService)
+	rest.Run(todoService)
 }
--- pkg/service/todo.go
+++ pkg/service/todo.go
@@ -3,13 +3,23 @@ package service
 import (
 	"github.com/patipolst/go-demo/pkg/mutation"
 	"github.com/patipolst/go-demo/pkg/query"
+	"github.com/patipolst/go-demo/pkg/store/db"
+	"github.com/patipolst/go-demo/pkg/store/memory"
 )
 
 type TodoService struct {
-	Query query.TodoQuery
+	Query    query.TodoQuery
 	Mutation mutation.TodoMutation
 }
 
-func NewTodoService(q query.TodoQuery, m mutation.TodoMutation) *TodoService {
+func NewTodoDBService(store *db.TodoStore) *TodoService {
+	q := query.NewTodoQuery(store)
+	m := mutation.NewTodoMutation(store)
+	return &TodoService{q, m}
+}
+
+func NewTodoMemoryService(store *memory.TodoStore) *TodoService {
+	q := query.NewTodoQuery(store)
+	m := mutation.NewTodoMutation(store)
 	return &TodoService{q, m}
 }
