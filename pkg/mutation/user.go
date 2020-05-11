4864146347f6a0f6bbed62c80f9e777b7da16140
patipol <patipol.st@gmail.com>
2020-05-11 16:07:36 +0700

    Add user query mutation

---
+++ pkg/mutation/user.go
@@ -0,0 +1,38 @@
+package mutation
+
+import "github.com/patipolst/go-demo/pkg/query"
+
+type NewUser struct {
+	Name int
+}
+
+type UserMutation interface {
+	CreateUser(NewUser) *query.User
+	CreateUsers([]NewUser) []*query.User
+}
+
+type UserStore interface {
+	CreateUser(NewUser) (*query.User, error)
+}
+
+type userMutation struct {
+	store UserStore
+}
+
+func NewUserMutation(store UserStore) UserMutation {
+	return &userMutation{store}
+}
+
+func (m *userMutation) CreateUser(user NewUser) *query.User {
+	u, _ := m.store.CreateUser(user)
+	return u
+}
+
+func (m *userMutation) CreateUsers(users []NewUser) []*query.User {
+	var created []*query.User
+	for _, user := range users {
+		u := m.CreateUser(user)
+		created = append(created, u)
+	}
+	return created
+}
--- pkg/query/user.go
+++ pkg/query/user.go
@@ -1,6 +1,32 @@
 package query
 
 type User struct {
-	ID   int    `json:"id"`
-	Name string `json:"name"`
+	ID   int
+	Name string
+}
+
+type UserQuery interface {
+	GetUsers() []*User
+	GetUser(int) (*User, error)
+}
+
+type UserStore interface {
+	GetAllUsers() []*User
+	GetUser(int) (*User, error)
+}
+
+type userQuery struct {
+	store UserStore
+}
+
+func NewUserQuery(store UserStore) UserQuery {
+	return &userQuery{store}
+}
+
+func (q *userQuery) GetUsers() []*User {
+	return q.store.GetAllUsers()
+}
+
+func (q *userQuery) GetUser(id int) (*User, error) {
+	return q.store.GetUser(id)
 }
